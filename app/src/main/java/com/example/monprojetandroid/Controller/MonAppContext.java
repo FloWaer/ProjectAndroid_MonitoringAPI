09be3c5fcb28aba5cf092629e0ad48dd1df34bb6
FloWaer <florent.waerenburgh@std.heh.be>
2020-10-29 12:13:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/monprojetandroid/Controller/MonAppContext.java
@@ -0,0 +1,21 @@
+package com.example.monprojetandroid.Controller;
+
+import android.app.Application;
+
+public class MonAppContext extends Application {
+    // variables globales de l'application
+    private int idLoginConnected;
+    public int getIdLoginConnected () { return idLoginConnected; }
+    public void setIdLoginConnected (int id) { idLoginConnected = id; }
+
+    private String userRole;
+    public String getUserRole () { return userRole; }
+    public void setUserRole (String id) { userRole = id; }
+
+    // initialisation du Contexte
+    @Override public void onCreate(){
+        super.onCreate();
+        idLoginConnected = -1; // mon nom est personne
+        userRole = "READ";
+    }
+}
---
+++ app/src/main/java/com/example/monprojetandroid/thread/IEnumPlc.java
@@ -0,0 +1,6 @@
+package com.example.monprojetandroid.thread;
+
+interface IEnumPlc {
+    public int getDb();
+    public int getDbb();
+}
